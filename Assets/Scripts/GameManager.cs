107ac3e5b13fc6ed820f1ba9f824a1eeac086579
boter116 <rrespici@sfu.ca>
2019-10-05 14:40:38 -0700

    inital commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    //Variable for the pillar prefab (Set in editor)
+    public GameObject Pillar;
+    
+    //Variable for the fade gameobject (set in editor)
+    public GameObject fade;
+    
+    public float MaxSpawnY = 3f;
+    public float MinSpawnY = -3f;
+    public float PillarSpeed = 3;
+    
+    //Text that shows your score
+    public Text ScoreText;
+    
+    //Text that shows up when you've lost
+    public GameObject GameOverText;
+    public GameObject RestartButton;
+    
+    //The players score
+    private int _score = 0;
+
+    //Interval between spawning blocks
+    private float _spawnTime = 2f;
+    
+    //A timer to track when to spawn blocks
+    private float _time = 0f;
+
+    //Variable to track whether the game is still being played or if the player lost.
+    private bool _gameOver = false;
+    
+    
+    
+    // Update is called once per frame
+    void Update() {
+        _time += Time.deltaTime;
+        if (_time >= _spawnTime) {
+            if (Pillar != null){
+                SpawnPillar();
+            }
+            _time = 0f;
+        }
+	
+        if(_gameOver && Input.anyKeyDown)
+            Restart();
+
+    }
+
+
+
+    
+    void SpawnPillar() {
+        float y = Random.Range(MinSpawnY, MaxSpawnY);
+        Vector2 pos = new Vector2(13, y);
+        GameObject newPillar = Instantiate(Pillar, pos,  Quaternion.identity) as GameObject;
+        newPillar.GetComponent<Wall>().SetSpeed(PillarSpeed);
+    }
+
+    
+    
+    
+    public void IncreaseScore() {
+        _score++;
+        ScoreText.text = _score.ToString();
+    }
+
+    
+    
+
+    public void GameOver() {
+        fade.SetActive(true);
+        GameOverText.SetActive(true);
+        RestartButton.SetActive(true);
+        _gameOver = true;
+    }
+
+
+    public void Restart() {
+        SceneManager.LoadScene(Application.loadedLevel);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    
+    public GameManager GameManager;
+    //The amount of force to apply when the jump button is pressed
+    public Vector2 Force = new Vector2(0, 6);
+
+    //Variable to hold the physics component of the player
+    private Rigidbody2D _rigid;
+
+    //Boolean letting us know whether the jump button was pressed
+    private bool _jump = false;
+
+    //The max and min height the player is allowed to go to
+    private float _minY;
+    private float _maxY;
+    
+    
+    // Start is called before the first frame update
+    void Awake() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    //Fixed update is called at set intervals every x seconds
+    void FixedUpdate() {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+    }
+
+    
+    
+    private void OnCollisionEnter2D(Collision2D other) {
+    }
+
+
+
+    void GameOver() {
+    }
+}
